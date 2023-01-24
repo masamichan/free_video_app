617af54d3fe179909b14b4f085f73fb7847c08e0
masamichan <positivemasa031@gmail.com>
2023-01-24 14:34:25 +0900

    make component of Analysis and adding css

---
+++ .history/src/components/Analysis/Analysis_20230124142137.jsx
@@ -0,0 +1,23 @@
+import React, { Component, Fragment } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+export class Analysis extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container>
+          <Row>
+            <Col lg={6} md={12} sm={12}>
+              <h1>BarCart</h1>
+            </Col>
+            <Col>
+              <h1>Text</h1>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default Analysis;
---
+++ .history/src/components/Analysis/Analysis_20230124142708.jsx
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+
+export class Analysis extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container className="text-center">
+          <h1 className="serviceMainTitle">TECHNOLOGY USED</h1>
+          <div className="bottom"></div>
+          <Row>
+            <Col lg={6} md={12} sm={12}>
+              <h1>BarCart</h1>
+            </Col>
+            <Col lg={6} md={12} sm={12}>
+              <h1>Text</h1>
+            </Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default Analysis;
