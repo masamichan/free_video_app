d25016aa91d8152e0cbc551d1d84e44fed747a8b
masamichan <positivemasa031@gmail.com>
2023-02-06 20:57:19 +0900

    make clientReview Component

---
+++ .history/src/components/ClientReview/ClientReview_20230206205205.jsx
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from "react";
+import { Col, Container } from "react-bootstrap";
+
+export class ClientReview extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container fluid={true} className="siderBack">
+          <Row>
+            <Col></Col>
+          </Row>
+        </Container>
+      </Fragment>
+    );
+  }
+}
+
+export default ClientReview;
